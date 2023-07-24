fb447b5923faad3cc0650e390423026b83d7349b
krishna-kachhad <krishnakachhad20@gmail.com>
2023-07-24 23:08:13 +0530

    lecture_practice

---
+++ Lecture_Practicle/type.py
@@ -0,0 +1,16 @@
+num=456
+print(num)
+print(type(num))
+
+name="python"
+print(type(name))
+
+price=4.5
+print(price)
+print(type(price))
+
+nums = [1,2,3,4]#list
+print(type(nums))
+
+nums_dict = {'one':1,'two':2,'three':3}#dictionary
+print(type(nums_dict))
