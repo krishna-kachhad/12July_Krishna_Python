e5ee962c6f424a345e2f525059115d9df9f1a5af
krishna-kachhad <krishnakachhad20@gmail.com>
2023-07-24 20:28:08 +0530

    lecture_practice

---
+++ Lecture_Practicle/end_sep.py
@@ -0,0 +1,6 @@
+print("hello",end=" ")
+print("everyone")
+print("hello",end=" &&& ")#output: hello &&& everyone
+print("everyone")
+
+print("hello","everyone","good noon",sep="|")#used for separate the string
