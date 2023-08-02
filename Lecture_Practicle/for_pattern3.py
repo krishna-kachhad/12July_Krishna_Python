ca6d7287de736149938dfc831b572cc439942017
krishna-kachhad <krishnakachhad20@gmail.com>
2023-08-02 20:12:15 +0530

    lecture_practice

--- Lecture_Practicle/for_pattern3.py
+++ Lecture_Practicle/for_pattern3.py
@@ -11,3 +11,18 @@ for r in range(1,6):
         print(r,end=" ") #column
     print(" ") #row break
 
+'''
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15
+'''
+
+num=1
+for r in range(1,6):
+    for c in range(1,r+1):
+        print(num,end=" ") #column
+        num=num+1
+    print("") #OR print("\r") \r used for (ending line after each row)
+
