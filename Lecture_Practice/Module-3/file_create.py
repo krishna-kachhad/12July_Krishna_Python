6c21ca7930060b0c1733032a0ffa7cc6e681d58f
krishna-kachhad <krishnakachhad20@gmail.com>
2023-08-26 17:36:45 +0530

    python_program

--- Lecture_Practice/Module-3/file_create.py
+++ Lecture_Practice/Module-3/file_create.py
@@ -1,5 +1,20 @@
-#open('test.txt', 'x') # x - mode for file creating
+'''open('test.txt', 'x') # x - mode for file creating
 
-#open('newfile.txt','w') # w - mode for file creating + write
+open('newfile.txt','w') # w - mode for file creating + write
 
-open ('hello.txt', 'a') # a - mode for file creating + append
+open ('hello.txt', 'a') # a - mode for file creating + append'''
+
+#----------------ppt example--------------
+# file demo
+
+file = open("test.txt","w")
+file.write("Hello\n")
+file.write("Welcome to file management program")
+file.close
+file = open("test.txt","r")
+str=file.readline()
+print(str)
+a=file.tell()
+print(a)
+str=file.read()
+print(str)
